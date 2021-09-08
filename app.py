554d8ab7aa255e182c35562000eb1f65e21807b5
Dan Benson <dbenson3091@gmail.com>
2021-09-08 18:55:46 -0500

    created routing for results after form submission

--- app.py
+++ app.py
@@ -9,6 +9,21 @@ app.config['SECRET_KEY'] = "secret"
 debug = DebugToolbarExtension(app)
 
 @app.get("/")
-def show_form():
+def display_mad_libs_prompt():
+    #Need more context of name display_mad_libs_prompt
+    """Render mad libs form for input"""
 
+    # Make variable and input it in the arguments of questions.html
     return render_template("questions.html",prompts=silly_story.prompts)
+
+@app.get('/results')
+
+def show_results():
+    """Create Dictionary from silly_story.prompts, render story.html of results using generate function of silly_story"""
+
+    #story_input = {prompt: request.args.get(prompt) for prompt in silly_story.prompts }
+
+    story_info = silly_story.generate(request.args)
+    return render_template("story.html", story=story_info)
+
+
